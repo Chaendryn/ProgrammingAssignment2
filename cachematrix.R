009a6b95029f4445ff15d4a7412200f12170b904
Yolande Korsten <chaendryn@gmail.com>
2014-05-25 16:24:28 +0200

    Reformatted comments for easier reading

--- cachematrix.R
+++ cachematrix.R
@@ -8,15 +8,17 @@
 ## is ## list containing four functions for matrix manipulation.
 
 makeCacheMatrix <- function(myMatrix = matrix()) { 
-      m <- NULL ## initialises an empty matrix which will be used to store the inverse matrix data
-      set <- function(y) { ## creates the 'set' accessor function, which sets the value of the matrix
-            myMatrix <<- y ## Makes the 'set' function available to the other functions outside the environment of this function 
-            m <<- NULL ## Makes the initialised inverse storage matrix available to the cacheSolve function 
+      m <- NULL               ## initialises an empty matrix which will be used to store the inverse matrix data
+      set <- function(y) {    ## creates the 'set' accessor function, which sets the value of the matrix
+            myMatrix <<- y    ## Makes the 'set' function available to the other functions outside the 
+                              ## environment of this function 
+            m <<- NULL        ## Makes the initialised inverse storage matrix available to the cacheSolve function 
       }
       get <- function() myMatrix ## Creates the 'get' accessor function, which gets the value of the matrix
-      setIMatrix <- function(solve) m <<- solve ## Sets the inverse matrix m, and makes the solve function available outside the scope of this function
+      setIMatrix <- function(solve) m <<- solve ## Sets the inverse matrix m, and makes the solve function 
+                                                ## available outside the scope of this function
       getIMatrix <- function() m ## gets the value of the inverse matrix
-      list(set = set, get = get, ## creats the function list with get, set, setIMatrix and getIMatrix
+      list(set = set, get = get, ## creates the function list with get, set, setIMatrix and getIMatrix
            setIMatrix = setIMatrix,
            getIMatrix = getIMatrix)
 }
@@ -35,11 +37,12 @@ cacheSolve <- function(myMatrix, ...) {
       ## a message is printed to the console.
       if(!is.null(m)) {
             message("cached data retrieved") ## sets the message to be printed to the console
-            return(m) ## prints the cached inverse matrix to the console
+            return(m)         ## prints the cached inverse matrix to the console
       }
-      data <- myMatrix$get() ## retrieves the variables for the matrix with which the solve function will calculate the inverse
-      m <- solve(data, ...) ## computes the inverse matrix from data retrieved and writes it to m
-      myMatrix$setIMatrix(m) ## Sets the values computed by the solve function run on myMatrix
-      m ## prints the inverse matrix to the console
+      data <- myMatrix$get()  ## retrieves the variables for the matrix with which the solve function will 
+                              ## calculate the inverse
+      m <- solve(data, ...)   ## computes the inverse matrix from data retrieved and writes it to m
+      myMatrix$setIMatrix(m)  ## Sets the values computed by the solve function run on myMatrix
+      m                       ## prints the inverse matrix to the console
       
 }
